ba64eb2e232fe18ca9e0ca1f45d7e383ca30ad1b
Brendan Moran <b.daniel.moran@gmail.com>
2021-03-22 20:15:45 -0400

    mapping out basic plan for api

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ app.get('/greetings', (req, res) => {
   res.json({ greeting: 'Yo, fuckas.' })
 });
 
-//send a POST request to CREATE a new quote
-//send a GET request to READ a quote
-//send a PUT request to UPDATE a quote
-//send a DELETE request to DELETE a quote
+
+//send a GET request to /quotes to READ a list of quotes
+//send a GET request to /quote/:id to READ a quote
+//send a POST request /quotes to CREATE a new quote
+//send a PUT request /quotes/:id to UPDATE a quote
+//send a DELETE request to quotes/:id to DELETE a quote
+//send a GET request to /quotes/quote/random READ a random quote
 
 app.listen(3000, () => console.log('Quote API listening on port 3000!'));
