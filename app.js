95cf59cc91644323a3a6d24f0498efd485a61295
Brendan Moran <b.daniel.moran@gmail.com>
2021-03-24 19:37:30 -0400

    refactored to use mini-orm and local data store

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const app = express();
 
+const records = require('./records');
+
 //send a GET request to /quotes to READ a list of quotes
-app.get('/quotes', (req, res) => {
-  res.json(data)
+app.get('/quotes', async (req, res) => {
+  const quotes = await records.getQuotes();
+  res.json(quotes);
 });
 //send a GET request to /quote/:id to READ a quote
-app.get('/quotes/:id', (req, res) => {
-  const quote = data.quotes.find(quote => quote.id == req.params.id);
-  res.json(quote)
+app.get('/quotes/:id', async (req, res) => {
+  const quote = await records.getQuote(req.params.id);
+  res.json(quote);
 });
 //send a POST request /quotes to CREATE a new quote
 //send a PUT request /quotes/:id to UPDATE a quote
@@ -17,22 +20,4 @@ app.get('/quotes/:id', (req, res) => {
 
 app.listen(3000, () => console.log('Quote API listening on port 3000!'));
 
-const data = {
-    quotes: [
-      {
-        id: 8721,
-        quote: "We must accept finite disappointment, but we must never lose infinite hope.",
-        author: "Martin Luther King"
-      },
-      {
-        id: 5779,
-        quote: "Use what you’ve been through as fuel, believe in yourself and be unstoppable!",
-        author: "Yvonne Pierre"
-      },
-      {
-        id: 3406,
-        quote: "To succeed, you have to do something and be very bad at it for a while. You have to look bad before you can look really good.",
-        author: "Barbara DeAngelis"
-      }
-    ]
-  }
+
