22a3b8ae9a77b957cdf88f2e1db33814ce8bfe42
Brendan Moran <b.daniel.moran@gmail.com>
2021-03-29 21:13:02 -0400

    set up to receive post requests as json from postman

--- app.js
+++ app.js
@@ -3,17 +3,28 @@ const app = express();
 
 const records = require('./records');
 
+app.use(express.json());
+
 //send a GET request to /quotes to READ a list of quotes
 app.get('/quotes', async (req, res) => {
   const quotes = await records.getQuotes();
   res.json(quotes);
 });
+
 //send a GET request to /quote/:id to READ a quote
 app.get('/quotes/:id', async (req, res) => {
   const quote = await records.getQuote(req.params.id);
   res.json(quote);
 });
+
 //send a POST request /quotes to CREATE a new quote
+app.post('/quotes', (req, res) => {
+  const quote = records.createQuote({
+    quote: req.body.quote,
+    author: req.body.author
+  });
+});
+
 //send a PUT request /quotes/:id to UPDATE a quote
 //send a DELETE request to quotes/:id to DELETE a quote
 //send a GET request to /quotes/quote/random READ a random quote
