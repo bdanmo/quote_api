6d9bd30da8ebdabb59b0cfb74da9f83b96a93acd
Brendan Moran <b.daniel.moran@gmail.com>
2021-04-07 11:53:25 -0400

    refactored with asyncHandler

--- app.js
+++ app.js
@@ -18,50 +18,36 @@ app.use(express.json());
 
 //send a GET request to /quotes to READ a list of quotes
 app.get('/quotes', asyncHandler(async(req, res, next) => {
-  try {
-    const quotes = await records.getQuotes();
-    res.json(quotes);
-  } catch (err) {
-    next(err);
-  }
+  const quotes = await records.getQuotes();
+  res.json(quotes);
 }));
 
 //send a GET request to /quote/:id to READ a quote
-app.get('/quotes/:id', async (req, res, next) => {
-  try {
-    const quote = await records.getQuote(req.params.id);
+app.get('/quotes/:id', asyncHandler(async(req, res, next) => {
+  const quote = await records.getQuote(req.params.id);
     if (quote) {
       res.json(quote);
     } else {
       res.status(404).json({ message: 'Quote not found.' });
     }
-  } catch (err) {
-    next(err);
-  }
-});
+}));
 
 //send a POST request /quotes to CREATE a new quote
-app.post('/quotes', async (req, res, next) => {
-  try {
-    if (req.body.author && req.body.quote) {
-      const quote = await records.createQuote({
-        quote: req.body.quote,
-        author: req.body.author
-      });
-      res.status(201).json(quote);
-    } else {
-      res.status(400).json({message: "Bad request. Must provide author and quote."})
-    }
-  } catch (err) {
-    next(err);
+app.post('/quotes', asyncHandler(async (req, res, next) => {
+  if (req.body.author && req.body.quote) {
+    const quote = await records.createQuote({
+      quote: req.body.quote,
+      author: req.body.author
+    });
+    res.status(201).json(quote);
+  } else {
+    res.status(400).json({message: "Bad request. Must provide author and quote."})
   }
-  
-});
+}));
 
 //send a PUT request /quotes/:id to UPDATE a quote
-app.put('/quotes/:id', async (req, res, next) => {
-  try {
-    const quote = await records.getQuote(req.params.id);
+app.put('/quotes/:id', asyncHandler(async (req, res, next) => {
+  const quote = await records.getQuote(req.params.id);
     if (quote) {
       quote.quote = req.body.author;
       quote.author = req.body.quote;
@@ -69,27 +55,20 @@ app.put('/quotes/:id', async (req, res, next) => {
       res.status(204).end();
     } else {
       res.status(404).json({message: 'Quote was not found.'})
-    }
-  } catch (err) {
-    next(err);
-  }
-  
-});
+    }  
+}));
+
 //send a DELETE request to quotes/:id to DELETE a quote
-app.delete('/quotes/:id', async (req, res, next) => {
-  try {
-    throw new Error("Shit just hit the fan.");
-    const quote = await records.getQuote(req.params.id);
-    if (quote) {
-      await records.deleteQuote(quote);
-      res.status(204).end();
-    } else {
-      res.status(404).json({ message: "There's no quote to delete!" });
-    }
-  } catch (err) {
-    next(err);
+app.delete('/quotes/:id', asyncHandler(async (req, res, next) => {
+  const quote = await records.getQuote(req.params.id);
+  if (quote) {
+    await records.deleteQuote(quote);
+    res.status(204).end();
+  } else {
+    res.status(404).json({ message: "There's no quote to delete!" });
   }
-})
+}));
+
 //send a GET request to /quotes/quote/random READ a random quote
 
 //catch requests sent to non-existent routes
